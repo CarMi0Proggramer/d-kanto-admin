53b29be1b197f0aea5c990cc1492964f6f6531be
Carlos <carlosmiguelgarcesbasulto@gmail.com>
2024-07-19 21:30:42 -0400

    a

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ function signIn() {
     }
 
     /* SIGNING IN */
-    fetch("https://d-kanto-backend.onrender.com/admins/signIn", {
+    fetch("http://localhost:3000/admins/signIn", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
